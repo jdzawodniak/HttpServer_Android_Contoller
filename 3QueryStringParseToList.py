f10e62a5ea0aab22c9c37d9b40bd1b077d57c66a
jdzawodniak <38980694+jdzawodniak@users.noreply.github.com>
2019-06-03 17:46:51 -0500

    Create 3QueryStringParseToList.py

---
+++ 3QueryStringParseToList.py
@@ -0,0 +1,34 @@
+################################
+##Generated with a lot of love##
+##    with   EasyPython       ##
+##Web site: easycoding.tn     ##
+################################
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+QueryString = None
+i = None
+querystringlist = None
+list2 = None
+
+class RequestHandler_httpd(BaseHTTPRequestHandler):
+  def do_GET(self):
+    global QueryString, i, querystringlist, list2
+    QueryString = self.requestline
+    QueryString = QueryString[5 : int(len(QueryString) - 9)]
+    print(QueryString)
+    for i in range(1, 7):
+      querystringlist = QueryString.split(',')
+      print(str(i) + str(list2[int(i - 1)]))
+    messagetosend = bytes(QueryString,"utf")
+    self.send_response(200)
+    self.send_header('Content-Type', 'text/plain')
+    self.send_header('Content-Length', len(messagetosend))
+    self.end_headers()
+    self.wfile.write(messagetosend)
+    return
+
+
+server_address_httpd = ('192.168.86.54',8080)
+httpd = HTTPServer(server_address_httpd, RequestHandler_httpd)
+print('Starting Server')
+httpd.serve_forever()
